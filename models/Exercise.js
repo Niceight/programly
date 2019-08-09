d9bfc060704533ae601c46c84722439ed0c32cf4
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-08-09 22:05:12 +0800

    Add Exercise model

---
+++ models/Exercise.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExerciseScheme = new Schema({
+  topicName: {
+    type: String,
+    required: true
+  },
+  topic: {
+    type: String,
+    required: true
+  },
+  question: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: String
+  },
+  lecturer: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  timestamps: true
+});
+
+module.exports = Exercise = mongoose.model("exercises", ExerciseScheme);
