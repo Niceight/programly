5eb031f1ccb7a8922cc7379588f09129f3b7fbf7
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-08-10 16:33:49 +0800

    Add useFindAndModify: false

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, { useFindAndModify: false })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
