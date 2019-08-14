a508ebe14fa0f7991d57a70094de10340a878b5a
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-08-15 03:08:56 +0800

    Add index to export all auth files

---
+++ client/src/Components/auth/index.js
@@ -0,0 +1,7 @@
+// Export Lecturer
+export { default as LoginLecturer } from "./lecturer/Login";
+export { default as RegisterLecturer } from "./lecturer/Register";
+
+// Export Student
+export { default as LoginStudent } from "./student/Login";
+export { default as RegisterStudent } from "./student/Register";
