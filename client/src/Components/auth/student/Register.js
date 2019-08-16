2fa42124f87246b59cb2f43822c3d5a2de229590
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-08-17 00:15:21 +0800

    Add redux to student

--- client/src/Components/auth/student/Register.js
+++ client/src/Components/auth/student/Register.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { registerStudent } from "../../../actions/authActions";
+
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -7,142 +13,254 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/styles";
 import Container from "@material-ui/core/Container";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {". Built with "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Material-UI.
-      </Link>
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles(theme => ({
+const styles = {
   "@global": {
     body: {
-      backgroundColor: theme.palette.common.white
+      background: "white"
     }
   },
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: 50,
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
   },
   avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    margin: 8,
+    background: "secondary"
   },
   form: {
     width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3)
+    marginTop: 8
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
+    margin: (24, 0, 16, 0)
   }
-}));
+};
 
-export default function SignUp() {
-  const classes = useStyles();
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstname: "",
+      lastname: "",
+      studentID: "",
+      programID: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newStudent = {
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      studentID: this.state.studentID,
+      programID: this.state.programID,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    this.props.registerStudent(newStudent, this.props.history);
+  }
+
+  render() {
+    const { errors } = this.state;
+    const { classes } = this.props;
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <form onSubmit={this.onSubmit} className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="firstname"
+                  name="firstname"
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  autoFocus
+                  autoComplete="fname"
+                  value={this.state.firstname}
+                  onChange={this.onChange}
+                  error={!!errors.firstname}
+                  helperText={errors.firstname}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="lastname"
+                  name="lastname"
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  autoComplete="lname"
+                  value={this.state.lastname}
+                  onChange={this.onChange}
+                  error={!!errors.lastname}
+                  helperText={errors.lastname}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="studentID"
+                  name="studentID"
+                  label="Student ID"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  autoComplete="studentID"
+                  value={this.state.studentID}
+                  onChange={this.onChange}
+                  error={!!errors.studentID}
+                  helperText={errors.studentID}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="programID"
+                  name="programID"
+                  label="Program ID"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  autoComplete="programID"
+                  value={this.state.programID}
+                  onChange={this.onChange}
+                  error={!!errors.programID}
+                  helperText={errors.programID}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="email"
+                  name="email"
+                  type="email"
+                  label="Email Address"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  autoComplete="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={!!errors.email}
+                  helperText={errors.email}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="password"
+                  name="password"
+                  type="password"
+                  label="Password"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  autoComplete="current-password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={!!errors.password}
+                  helperText={errors.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="password2"
+                  name="password2"
+                  type="password"
+                  label="Password Confirmation"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  value={this.state.password2}
+                  onChange={this.onChange}
+                  error={!!errors.password2}
+                  helperText={errors.password2}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Register
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="/students/login" variant="body2">
+                  Already have an account? Login
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
+          </form>
+        </div>
+      </Container>
+    );
+  }
 }
+
+Register.propTypes = {
+  classes: PropTypes.object.isRequired,
+  registerStudent: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { registerStudent }
+)(withRouter(withStyles(styles)(Register)));
