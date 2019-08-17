0951dd6f80b980168c4ee8c442d55dae10263777
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-08-17 16:45:25 +0800

    Redirect logout to homepage

--- client/src/Components/Layout/Landing.js
+++ client/src/Components/Layout/Landing.js
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/styles";
 import Container from "@material-ui/core/Container";
 
-const useStyles = makeStyles(theme => ({
+const styles = {
   "@global": {
     body: {
-      backgroundColor: theme.palette.common.white
+      background: "white"
     }
   },
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: 100,
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
   }
-}));
+};
 
-export default function SignIn() {
-  const classes = useStyles();
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h1">
-          programly
-        </Typography>
-      </div>
-    </Container>
-  );
+class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h1">
+            programly
+          </Typography>
+        </div>
+      </Container>
+    );
+  }
 }
+
+Landing.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(withStyles(styles)(Landing));
