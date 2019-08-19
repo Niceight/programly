b4f18dc22661626b41153946abbb9d940fd65e8a
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-08-19 14:47:28 +0800

    Rename folder Layout to layout

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./Components/Theme/Theme";
-import { AppBar, Landing, Footer } from "./Components/Layout";
+import { AppBar, Landing, Footer } from "./Components/layout";
 import {
   LoginLecturer,
   RegisterLecturer,
