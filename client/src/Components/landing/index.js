69473e2819050299dc87bbb7dd11012426b7d532
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-08-22 07:12:59 +0800

    Add index for landing folder

---
+++ client/src/Components/landing/index.js
@@ -0,0 +1,3 @@
+export { default as ButtonLecturer } from "./ButtonLecturer";
+export { default as ButtonStudents } from "./ButtonStudent";
+export { default as LinkProgramly } from "./LinkProgramly";
