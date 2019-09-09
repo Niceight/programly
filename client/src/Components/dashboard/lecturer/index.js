6fc2c18f3d7b761a18aae578390545c2d5db4add
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-09-09 23:18:26 +0800

    Add stuff

--- client/src/Components/dashboard/lecturer/index.js
+++ client/src/Components/dashboard/lecturer/index.js
@@ -1,2 +1,5 @@
 export { default as DashboardLecturer } from "./Dashboard";
 export { default as LinkDashboard } from "./LinkDashboard";
+export { default as CreateExercise } from "./exercise/CreateExercise";
+export { default as ListDrawer } from "./ListDrawer";
+export { default as Exercises } from "./exercise/Exercises";
