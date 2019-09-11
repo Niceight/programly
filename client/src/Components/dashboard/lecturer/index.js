502075fb2c8c0afdaa91113c85376913358358b2
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-09-11 23:13:50 +0800

    Add export EditExercise

--- client/src/Components/dashboard/lecturer/index.js
+++ client/src/Components/dashboard/lecturer/index.js
@@ -3,3 +3,4 @@ export { default as LinkDashboard } from "./LinkDashboard";
 export { default as CreateExercise } from "./exercise/CreateExercise";
 export { default as ListDrawer } from "./ListDrawer";
 export { default as Exercises } from "./exercise/Exercises";
+export { default as EditExercise } from "./exercise/EditExercise";
