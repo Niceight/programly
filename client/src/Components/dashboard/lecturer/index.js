ca0ffaa1bf7f6c147684f251f0a3ac4adbccae3a
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-09-17 01:27:39 +0800

    Export classroom

--- client/src/Components/dashboard/lecturer/index.js
+++ client/src/Components/dashboard/lecturer/index.js
@@ -4,3 +4,6 @@ export { default as CreateExercise } from "./exercise/CreateExercise";
 export { default as ListDrawer } from "./ListDrawer";
 export { default as Exercises } from "./exercise/Exercises";
 export { default as EditExercise } from "./exercise/EditExercise";
+export { default as Classrooms } from "./classroom/Classrooms";
+export { default as CreateClassroom } from "./classroom/CreateClassroom";
+export { default as EditClassroom } from "./classroom/EditClassroom";
