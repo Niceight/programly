613544a80912ef4dbabf4665b4a5eb2bd669ec50
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-09-17 01:25:50 +0800

    Add classroom actions

---
+++ client/src/actions/classroomActions.js
@@ -0,0 +1,113 @@
+import axios from "axios";
+
+import {
+  GET_CLASSROOM,
+  GET_CLASSROOMS,
+  UPDATE_CLASSROOM,
+  CLASSROOM_LOADING,
+  CLEAR_CURRENT_CLASSROOM,
+  GET_ERRORS
+} from "./types";
+
+// Get current class
+export const getCurrentClassroom = (userid, classroomid) => dispatch => {
+  dispatch(setClassroomLoading());
+  axios
+    .get(`/api/classrooms/${userid}/${classroomid}`)
+    .then(res =>
+      dispatch({
+        type: GET_CLASSROOM,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_CLASSROOM,
+        payload: {}
+      })
+    );
+};
+
+// Create Classroom
+export const createClassroom = classroomData => dispatch => {
+  axios
+    .post("/api/classrooms/new-classroom", classroomData)
+    .then(res =>
+      dispatch({
+        type: GET_CLASSROOM,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Get all classs
+export const getClassrooms = id => dispatch => {
+  dispatch(setClassroomLoading());
+  axios
+    .get(`/api/classrooms/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_CLASSROOMS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_CLASSROOMS,
+        payload: null
+      })
+    );
+};
+
+// Update Classroom
+export const updateClassroom = (classroomid, classroomData) => {
+  return dispatch => {
+    axios
+      .post(`/api/classrooms/${classroomid}`, classroomData)
+      .then(res => {
+        dispatch({
+          type: UPDATE_CLASSROOM,
+          payload: res.data
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        });
+      });
+  };
+};
+
+// Delete Classroom
+export const deleteClassroom = (userid, classroomid) => dispatch => {
+  axios
+    .delete(`/api/classrooms/${userid}/${classroomid}`)
+    .then(res => dispatch(getClassrooms(userid)))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Classroom loading
+export const setClassroomLoading = () => {
+  return {
+    type: CLASSROOM_LOADING
+  };
+};
+
+// Clear classroom
+export const clearCurrentClassroom = () => {
+  return {
+    type: CLEAR_CURRENT_CLASSROOM
+  };
+};
