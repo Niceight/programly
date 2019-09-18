850eb0847504e27e746299af33dcd69c49325b86
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-09-18 23:45:32 +0800

    Rename ListDrawer to ListDrawerLecturer

--- client/src/Components/dashboard/lecturer/index.js
+++ client/src/Components/dashboard/lecturer/index.js
@@ -1,7 +1,7 @@
 export { default as DashboardLecturer } from "./Dashboard";
 export { default as LinkDashboard } from "./LinkDashboard";
 export { default as CreateExercise } from "./exercise/CreateExercise";
-export { default as ListDrawer } from "./ListDrawer";
+export { default as ListDrawerLecturer } from "./ListDrawer";
 export { default as Exercises } from "./exercise/Exercises";
 export { default as EditExercise } from "./exercise/EditExercise";
 export { default as Classrooms } from "./classroom/Classrooms";
