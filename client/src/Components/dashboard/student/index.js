78871f88b6cfe2186fbd6772b7e961169e39f489
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-09-25 02:08:54 +0800

    Export JoinClassroom

--- client/src/Components/dashboard/student/index.js
+++ client/src/Components/dashboard/student/index.js
@@ -1,3 +1,4 @@
 export { default as DashboardStudent } from "./Dashboard";
 export { default as LinkDashboard } from "./LinkDashboard";
 export { default as ListDrawerStudent } from "./ListDrawer";
+export { default as JoinClassroom } from "./classroom/JoinClassroom";
