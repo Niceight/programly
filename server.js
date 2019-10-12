910af1da6a56e7e854bfa242deb852d2e9e59bd7
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-10-13 01:55:32 +0800

    Add socket.io

--- server.js
+++ server.js
@@ -7,12 +7,27 @@ const lecturers = require("./routes/api/lecturers");
 const students = require("./routes/api/students");
 const classrooms = require("./routes/api/classrooms");
 const exercises = require("./routes/api/exercises");
+const progress = require("./routes/api/progression");
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Socket.io
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("User Disconnected");
+  });
+  socket.on("example_message", function(msg) {
+    console.log("message: " + msg);
+  });
+});
+io.listen(8000);
 
 // DB config
 const db = require("./config/keys").mongoURI;
@@ -34,6 +49,7 @@ app.use("/api/lecturers", lecturers);
 app.use("/api/students", students);
 app.use("/api/classrooms", classrooms);
 app.use("/api/exercises", exercises);
+app.use("/api/progress", progress);
 
 const port = process.env.PORT || 5000;
 
