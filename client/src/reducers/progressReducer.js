cc92d449e7e926d8d20bfc6d0d5806971de7a016
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-10-14 01:16:58 +0800

    Add progressReducer

---
+++ client/src/reducers/progressReducer.js
@@ -0,0 +1,41 @@
+import {
+  GET_PROGRESS,
+  GET_PROGRESSES,
+  PROGRESS_LOADING,
+  CLEAR_CURRENT_PROGRESS
+} from "../actions/types";
+
+const initialState = {
+  progress: null,
+  progresses: null,
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case PROGRESS_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_PROGRESS:
+      return {
+        ...state,
+        progress: action.payload,
+        loading: false
+      };
+    case GET_PROGRESSES:
+      return {
+        ...state,
+        progresss: action.payload,
+        loading: false
+      };
+    case CLEAR_CURRENT_PROGRESS:
+      return {
+        ...state,
+        progress: null
+      };
+    default:
+      return state;
+  }
+}
