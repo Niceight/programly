1ba65c21ef6bfc4fea4a550809a412609d5a0976
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-10-17 03:21:16 +0800

    Add socket.io

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const http = require("http");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
@@ -14,20 +15,6 @@ const app = express();
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Socket.io
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("disconnect", function() {
-    console.log("User Disconnected");
-  });
-  socket.on("example_message", function(msg) {
-    console.log("message: " + msg);
-  });
-});
-io.listen(8000);
 
 // DB config
 const db = require("./config/keys").mongoURI;
@@ -53,4 +40,52 @@ app.use("/api/progress", progress);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// Socket.io
+
+var server = http.createServer(app);
+var io = require("socket.io").listen(server);
+
+server.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server running on port ${port}`);
+  }
+});
+
+io.on("connection", socket => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("room", function(data) {
+    console.log("in joining room in SERVER");
+    socket.join(data.room);
+    console.log(data);
+    socket.broadcast.to(data.room).emit("load users and content");
+    socket.broadcast.to(data.room).emit("new user join", data.user);
+  });
+
+  socket.on("leave room", function(data) {
+    socket.broadcast.to(data.room).emit("user left room", { user: data.user });
+    socket.leave(data.room);
+  });
+
+  socket.on("coding event", function(data) {
+    console.log("in EXPRESS coding event");
+    console.log(data);
+    socket.broadcast.to(data.room).emit("receive content", {
+      content: data.content,
+      currentlyTyping: data.currentlyTyping
+    });
+  });
+  socket.on("change mode", function(data) {
+    socket.broadcast.to(data.room).emit("receive change mode", data.mode);
+  });
+
+  socket.on("send users and content", function(data) {
+    socket.broadcast.to(data.room).emit("receive users and content", data);
+  });
+});
