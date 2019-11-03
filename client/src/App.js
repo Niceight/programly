442c6535ad6a13d3b72660ee83eb2a47b3f2a59d
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-11-03 23:22:57 +0800

    Add notistack

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
+import { SnackbarProvider } from "notistack";
 import store from "./store";
 import PrivateRoute from "./Components/common/PrivateRoute";
 import jwt_decode from "jwt-decode";
@@ -81,217 +82,222 @@ if (localStorage.jwtToken) {
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <ThemeProvider theme={theme}>
-              <Root config={config} style={{ minHeight: "100vh" }}>
-                <CssBaseline />
-                <Header
-                  menuIcon={{
-                    inactive: <MenuIcon />,
-                    active: <ChevronLeftIcon />
-                  }}
-                >
-                  <Route
-                    exact
-                    path={[
-                      "/",
-                      "/lecturers/login",
-                      "/lecturers/register",
-                      "/students/login",
-                      "/students/register"
-                    ]}
-                    component={LinkProgramly}
-                  />
-
-                  <Switch>
-                    <PrivateRoute
-                      path={[
-                        "/students/dashboard",
-                        "/classrooms/join-classroom",
-                        "/myClassrooms/:id",
-                        "/myClassrooms/classroom/:id",
-                        "/myClassrooms/classroom/students/:id",
-                        "/myClassrooms/classroom/student/classroomid/:studentid",
-                        "/myClassrooms/classroom/progress/:studentid/:exerciseid",
-                        "/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
-                      ]}
-                      component={LinkStudent}
-                    />
-                    <PrivateRoute
-                      path={[
-                        "/lecturers/dashboard",
-                        "/exercises/new-exercise",
-                        "/exercises/:id",
-                        "/classrooms/new-classroom",
-                        "/classrooms/:id"
-                      ]}
-                      component={LinkLecturer}
-                    />
-                  </Switch>
-
-                  <Route
-                    path={[
-                      "/lecturers/dashboard",
-                      "/exercises/new-exercise",
-                      "/exercises/:id",
-                      "/classrooms/new-classroom",
-                      "/classrooms/:id",
-                      "/students/dashboard",
-                      "/myClassrooms/:id",
-                      "/myClassrooms/classroom/:id",
-                      "/myClassrooms/classroom/students/:id",
-                      "/myClassrooms/classroom/student/classroomid/:studentid",
-                      "/myClassrooms/classroom/progress/:studentid/:exerciseid",
-                      "/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
-                    ]}
-                    component={LogoutButton}
-                  />
-
-                  <Route
-                    exact
-                    path={[
-                      "/",
-                      "/lecturers/login",
-                      "/lecturers/register",
-                      "/students/login",
-                      "/students/register"
-                    ]}
-                    component={ButtonStudents}
-                  />
-                  <Route
-                    exact
-                    path={[
-                      "/",
-                      "/lecturers/login",
-                      "/lecturers/register",
-                      "/students/login",
-                      "/students/register"
-                    ]}
-                    component={ButtonLecturer}
-                  />
-                </Header>
-                <Nav
-                  collapsedIcon={{
-                    inactive: <ChevronLeftIcon />,
-                    active: <ChevronRightIcon />
-                  }}
-                  header={
-                    // you can provide fixed header inside nav
-                    // change null to some react element
-                    ctx => null
-                  }
-                >
-                  <Switch>
-                    <PrivateRoute
-                      path={[
-                        "/students/dashboard",
-                        "/classrooms/join-classroom",
-                        "/myClassrooms/:id",
-                        "/myClassrooms/classroom/:id",
-                        "/myClassrooms/classroom/students/:id",
-                        "/myClassrooms/classroom/student/classroomid/:studentid",
-                        "/myClassrooms/classroom/progress/:studentid/:exerciseid",
-                        "/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
-                      ]}
-                      component={ListDrawerStudent}
-                    />
-                    <PrivateRoute
-                      path={[
-                        "/lecturers/dashboard",
-                        "/exercises/new-exercise",
-                        "/exercises/:id",
-                        "/classrooms/new-classroom",
-                        "/classrooms/:id"
-                      ]}
-                      component={ListDrawerLecturer}
-                    />
-                  </Switch>
-                </Nav>
-                <Content>
-                  <Route exact path="/" component={Landing} />
-                  <Route path="/lecturers/login" component={LoginLecturer} />
-                  <Route
-                    path="/lecturers/register"
-                    component={RegisterLecturer}
-                  />
-                  <Route path="/students/login" component={LoginStudent} />
-                  <Route
-                    path="/students/register"
-                    component={RegisterStudent}
-                  />
-                  <Switch>
-                    <PrivateRoute
-                      path="/lecturers/dashboard"
-                      component={DashboardLecturer}
-                    />
-                    <PrivateRoute
-                      path="/exercises/new-exercise"
-                      component={CreateExercise}
-                    />
-                    <PrivateRoute
-                      path="/exercises/:lecturerID/:exerciseID"
-                      component={EditExercise}
-                    />
-                    <PrivateRoute
-                      path="/classrooms/new-classroom"
-                      component={CreateClassroom}
-                    />
-                    <PrivateRoute
-                      path="/classrooms/:lecturerID/:classroomID"
-                      component={EditClassroom}
-                    />
-                    <PrivateRoute path="/exercises/:id" component={Exercises} />
-                    <PrivateRoute
-                      path="/classrooms/join-classroom"
-                      component={JoinClassroom}
-                    />
-                    <PrivateRoute
-                      path="/classrooms/:id"
-                      component={Classrooms}
-                    />
-                    <PrivateRoute
+      <SnackbarProvider preventDuplicate>
+        <Provider store={store}>
+          <Router>
+            <div className="App">
+              <ThemeProvider theme={theme}>
+                <Root config={config} style={{ minHeight: "100vh" }}>
+                  <CssBaseline />
+                  <Header
+                    menuIcon={{
+                      inactive: <MenuIcon />,
+                      active: <ChevronLeftIcon />
+                    }}
+                  >
+                    <Route
                       exact
-                      path="/students/dashboard"
-                      component={DashboardStudent}
+                      path={[
+                        "/",
+                        "/lecturers/login",
+                        "/lecturers/register",
+                        "/students/login",
+                        "/students/register"
+                      ]}
+                      component={LinkProgramly}
                     />
-                    <PrivateRoute
-                      path="/classrooms/join-classroom"
-                      component={JoinClassroom}
+
+                    <Switch>
+                      <PrivateRoute
+                        path={[
+                          "/students/dashboard",
+                          "/classrooms/join-classroom",
+                          "/myClassrooms/:id",
+                          "/myClassrooms/classroom/:id",
+                          "/myClassrooms/classroom/students/:id",
+                          "/myClassrooms/classroom/student/classroomid/:studentid",
+                          "/myClassrooms/classroom/progress/:studentid/:exerciseid",
+                          "/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
+                        ]}
+                        component={LinkStudent}
+                      />
+                      <PrivateRoute
+                        path={[
+                          "/lecturers/dashboard",
+                          "/exercises/new-exercise",
+                          "/exercises/:id",
+                          "/classrooms/new-classroom",
+                          "/classrooms/:id"
+                        ]}
+                        component={LinkLecturer}
+                      />
+                    </Switch>
+
+                    <Route
+                      path={[
+                        "/lecturers/dashboard",
+                        "/exercises/new-exercise",
+                        "/exercises/:id",
+                        "/classrooms/new-classroom",
+                        "/classrooms/:id",
+                        "/students/dashboard",
+                        "/myClassrooms/:id",
+                        "/myClassrooms/classroom/:id",
+                        "/myClassrooms/classroom/students/:id",
+                        "/myClassrooms/classroom/student/classroomid/:studentid",
+                        "/myClassrooms/classroom/progress/:studentid/:exerciseid",
+                        "/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
+                      ]}
+                      component={LogoutButton}
                     />
-                    <PrivateRoute
-                      path="/myClassrooms/classroom/progress/:studentid/:exerciseid"
-                      component={Progress}
+
+                    <Route
+                      exact
+                      path={[
+                        "/",
+                        "/lecturers/login",
+                        "/lecturers/register",
+                        "/students/login",
+                        "/students/register"
+                      ]}
+                      component={ButtonStudents}
                     />
-                    <PrivateRoute
-                      path="/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
-                      component={Collaborate}
+                    <Route
+                      exact
+                      path={[
+                        "/",
+                        "/lecturers/login",
+                        "/lecturers/register",
+                        "/students/login",
+                        "/students/register"
+                      ]}
+                      component={ButtonLecturer}
                     />
-                    <PrivateRoute
-                      path="/myClassrooms/classroom/student/:classroomid/:studentid"
-                      component={Student}
+                  </Header>
+                  <Nav
+                    collapsedIcon={{
+                      inactive: <ChevronLeftIcon />,
+                      active: <ChevronRightIcon />
+                    }}
+                    header={
+                      // you can provide fixed header inside nav
+                      // change null to some react element
+                      ctx => null
+                    }
+                  >
+                    <Switch>
+                      <PrivateRoute
+                        path={[
+                          "/students/dashboard",
+                          "/classrooms/join-classroom",
+                          "/myClassrooms/:id",
+                          "/myClassrooms/classroom/:id",
+                          "/myClassrooms/classroom/students/:id",
+                          "/myClassrooms/classroom/student/classroomid/:studentid",
+                          "/myClassrooms/classroom/progress/:studentid/:exerciseid",
+                          "/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
+                        ]}
+                        component={ListDrawerStudent}
+                      />
+                      <PrivateRoute
+                        path={[
+                          "/lecturers/dashboard",
+                          "/exercises/new-exercise",
+                          "/exercises/:id",
+                          "/classrooms/new-classroom",
+                          "/classrooms/:id"
+                        ]}
+                        component={ListDrawerLecturer}
+                      />
+                    </Switch>
+                  </Nav>
+                  <Content>
+                    <Route exact path="/" component={Landing} />
+                    <Route path="/lecturers/login" component={LoginLecturer} />
+                    <Route
+                      path="/lecturers/register"
+                      component={RegisterLecturer}
                     />
-                    <PrivateRoute
-                      path="/myClassrooms/classroom/students/:id"
-                      component={Students}
+                    <Route path="/students/login" component={LoginStudent} />
+                    <Route
+                      path="/students/register"
+                      component={RegisterStudent}
                     />
-                    <PrivateRoute
-                      path="/myClassrooms/classroom/:id"
-                      component={Classroom}
-                    />
-                    <PrivateRoute
-                      path="/myClassrooms/:id"
-                      component={MyClassrooms}
-                    />
-                  </Switch>
-                </Content>
-                <Footer>{/* footer goes here */}</Footer>
-              </Root>
-            </ThemeProvider>
-          </div>
-        </Router>
-      </Provider>
+                    <Switch>
+                      <PrivateRoute
+                        path="/lecturers/dashboard"
+                        component={DashboardLecturer}
+                      />
+                      <PrivateRoute
+                        path="/exercises/new-exercise"
+                        component={CreateExercise}
+                      />
+                      <PrivateRoute
+                        path="/exercises/:lecturerID/:exerciseID"
+                        component={EditExercise}
+                      />
+                      <PrivateRoute
+                        path="/classrooms/new-classroom"
+                        component={CreateClassroom}
+                      />
+                      <PrivateRoute
+                        path="/classrooms/:lecturerID/:classroomID"
+                        component={EditClassroom}
+                      />
+                      <PrivateRoute
+                        path="/exercises/:id"
+                        component={Exercises}
+                      />
+                      <PrivateRoute
+                        path="/classrooms/join-classroom"
+                        component={JoinClassroom}
+                      />
+                      <PrivateRoute
+                        path="/classrooms/:id"
+                        component={Classrooms}
+                      />
+                      <PrivateRoute
+                        exact
+                        path="/students/dashboard"
+                        component={DashboardStudent}
+                      />
+                      <PrivateRoute
+                        path="/classrooms/join-classroom"
+                        component={JoinClassroom}
+                      />
+                      <PrivateRoute
+                        path="/myClassrooms/classroom/progress/:studentid/:exerciseid"
+                        component={Progress}
+                      />
+                      <PrivateRoute
+                        path="/myClassrooms/classroom/collaborate/:progressid/:exerciseid"
+                        component={Collaborate}
+                      />
+                      <PrivateRoute
+                        path="/myClassrooms/classroom/student/:classroomid/:studentid"
+                        component={Student}
+                      />
+                      <PrivateRoute
+                        path="/myClassrooms/classroom/students/:id"
+                        component={Students}
+                      />
+                      <PrivateRoute
+                        path="/myClassrooms/classroom/:id"
+                        component={Classroom}
+                      />
+                      <PrivateRoute
+                        path="/myClassrooms/:id"
+                        component={MyClassrooms}
+                      />
+                    </Switch>
+                  </Content>
+                  <Footer>{/* footer goes here */}</Footer>
+                </Root>
+              </ThemeProvider>
+            </div>
+          </Router>
+        </Provider>
+      </SnackbarProvider>
     );
   }
 }
