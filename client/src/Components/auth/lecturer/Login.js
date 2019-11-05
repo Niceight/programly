b9edb2f54d68caacc8ad686b625d3a1bbcec97da
Dzulkarnain Mohamed <yuukeiv@gmail.com>
2019-11-05 22:05:50 +0800

    Fix material-ui

--- client/src/Components/auth/lecturer/Login.js
+++ client/src/Components/auth/lecturer/Login.js
@@ -7,39 +7,37 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/styles";
+import { withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
-const styles = {
+const styles = theme => ({
   "@global": {
     body: {
-      background: "white"
+      backgroundColor: theme.palette.common.white
     }
   },
   paper: {
-    marginTop: 50,
+    marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
   },
   avatar: {
-    margin: 8,
-    background: "secondary"
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: 8
+    width: "100%",
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: (24, 0, 16, 0)
+    margin: theme.spacing(3, 0, 2)
   }
-};
+});
 
 class Login extends Component {
   constructor() {
@@ -132,10 +130,6 @@ class Login extends Component {
                 />
               </Grid>
             </Grid>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
@@ -145,12 +139,7 @@ class Login extends Component {
             >
               Login
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
+            <Grid container justify="flex-end">
               <Grid item>
                 <Link href="/lecturers/register" variant="body2">
                   Don't have an account? Register
@@ -165,10 +154,9 @@ class Login extends Component {
 }
 
 Login.propTypes = {
-  classes: PropTypes.object.isRequired,
-  loginLecturer: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  loginLecturer: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
--- client/src/Components/auth/student/Register.js
+++ client/src/Components/auth/student/Register.js
@@ -7,39 +7,37 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/styles";
+import { withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
-const styles = {
+const styles = theme => ({
   "@global": {
     body: {
-      background: "white"
+      backgroundColor: theme.palette.common.white
     }
   },
   paper: {
-    marginTop: 50,
+    marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
   },
   avatar: {
-    margin: 8,
-    background: "secondary"
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: 8
+    width: "100%",
+    marginTop: theme.spacing(3)
   },
   submit: {
-    margin: (24, 0, 16, 0)
+    margin: theme.spacing(3, 0, 2)
   }
-};
+});
 
 class Register extends Component {
   constructor() {
@@ -214,14 +212,6 @@ class Register extends Component {
                   helperText={errors.password2}
                 />
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
             </Grid>
             <Button
               type="submit"
@@ -247,10 +237,9 @@ class Register extends Component {
 }
 
 Register.propTypes = {
-  classes: PropTypes.object.isRequired,
-  registerStudent: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  registerStudent: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
